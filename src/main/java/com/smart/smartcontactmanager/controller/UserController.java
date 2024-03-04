1396f2f557eb41319709f2e257d00e59ec0d4fac
Manas Baranwal <12345manasbrnwl@gmail.com>
2024-03-04 11:09:55 +0530

    git commit -am "changes"

--- src/main/java/com/smart/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smart/smartcontactmanager/controller/UserController.java
@@ -5,10 +5,13 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.smartcontactmanager.dao.UserRepository;
 import com.smart.smartcontactmanager.entities.User;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user/")
@@ -17,10 +20,22 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/dashboard")
-    public String dashboard(Model model, Principal principal) {
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
         User user = userRepository.getuserByUserName(principal.getName());
         model.addAttribute("user", user);
+    }
+
+    @RequestMapping("/dashboard")
+    public String dashboard(Model model) {
+        model.addAttribute("title", "Profile - Smart Contact Manager");
         return "normal\\dashboard";
     }
+
+    @GetMapping("/add-contact")
+    public String oenAddContactForm(Model model) {
+        model.addAttribute("title", "Add Contact");
+        return "normal\\add_contact";
+    }
+
 }
